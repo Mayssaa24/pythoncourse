03863c06f6262cda832e8231c6a113f2ec854cdd
Nemer Nour Mayssaa <nemernourmayssaa@gmail.com>
2026-09-22 18:01:09 +0200

    update

--- week1/task1.py
+++ week1/task1.py
@@ -1,6 +1,6 @@
-from math import pi 
-# Press the green button in the gutter to run the script. 
-if __name__ == '__main__': 
-r = float(input("Input the radius of the circle : ")) 
-print("The area of the circle with radius " + str(r) + " is: " + str(pi 
-* r ** 2))
+from math import pi
+
+if __name__ == '__main__':
+    r = float(input("Input the radius of the circle: "))
+    print("The area of the circle with radius " + str(r) +
+          " is: " + str(pi * r ** 2))
---
+++ week1/task2.py
@@ -0,0 +1,16 @@
+zahl = 10
+kommazahl = 10.5
+text = "Hello, World!"
+wahrheitswert = True
+
+print("Integer value:", zahl)
+print("Type:", type(zahl))
+
+print("Floating-point value:", kommazahl)
+print("Type:", type(kommazahl))
+
+print("String value:", text)
+print("Type:", type(text))
+
+print("Boolean value:", wahrheitswert)
+print("Type:", type(wahrheitswert))
