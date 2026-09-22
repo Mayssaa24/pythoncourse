78dc6035fefe1f0680963b26c1704fc70703161f
Nemer Nour Mayssaa <nemernourmayssaa@gmail.com>
2026-09-22 18:02:47 +0200

    update

---
+++ week1/task3.py
@@ -0,0 +1,18 @@
+zahl = 10
+float_zahl = float(zahl)
+
+kommazahl = 10.5
+int_kommazahl = int(kommazahl)
+
+string_zahl = str(zahl)
+
+text_nummer = "25"
+int_text = int(text_nummer)
+
+bool_zahl = bool(zahl)
+
+print("Integer to float:", float_zahl, type(float_zahl))
+print("Float to integer:", int_kommazahl, type(int_kommazahl))
+print("Integer to string:", string_zahl, type(string_zahl))
+print("String to integer:", int_text, type(int_text))
+print("Integer to Boolean:", bool_zahl, type(bool_zahl))
